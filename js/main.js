2f103f46d44df15fb75fe9136457f08bd43456a4
bchpete <96666546+emberswap@users.noreply.github.com>
2022-05-01 01:31:23 +0200

    change pic on theme cookie

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ if (theme) {
     $("html").attr("data-theme", theme);
 }
 $(document).ready(function(){
+    let current_theme = $("html").attr("data-theme");
+    if (current_theme == "dark"){
+        document.body.classList.toggle("group204")
+        if(document.body.classList.contains("group204")){
+            icon.src ="img/Group 204.png"
+        }
+    }
+    if (current_theme == "light"){
+        document.body.classList.toggle("group204")
+        if(document.body.classList.contains("group204")){
+            icon.src ="img/Group 205.png"
+        }
+    }
+});
+$(document).ready(function(){
     $(".theme-switcher").on('click', function(){
         $(".theme-switcher").toggleClass("flip");
         $(".theme-switcher").toggleClass("fa-sun");
@@ -15,7 +30,6 @@ $(document).ready(function(){
             localStorage.setItem('theme', 'light')
             if(document.body.classList.contains("group204")){
                 icon.src ="img/Group 205.png"
-                
             }
         }
         if (current_theme == "light"){
